00ba641365a45b20178e82ba82de16c7d26787e6
asukakuwahara <kuwahara.mee@gmail.com>
2026-07-03 10:22:30 +0200

    add bash file for hpc

---
+++ util/submit_train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name="mca-irony"
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --threads-per-core=1
+#SBATCH --mem=15000
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:A100:1
+spack load miniconda
+source .env
+
+python -m venv ~/envs/irony
+source ~/envs/irony/bin/activate
+pip install -r requirements.txt
+hf auth login --token HF_TOKEN
+python run_inference.py
