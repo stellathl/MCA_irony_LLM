2e16a099df53b203ca370342a9d39c6270e6c795
Asuka Kuwahara <42548220+asukakuwahara@users.noreply.github.com>
2026-07-05 09:01:23 +0200

    Update reasoning parse (#30)
    * latin square seeds start to be fixed
    
    * run inference after updating the reasoning answer
    
    * run reasoning
    
    * run general tempalte just in case

--- util/constants.py
+++ util/constants.py
@@ -15,7 +15,6 @@ PROMPT_FILES = {
     "rsa": "rsa_prompt.yaml",
     "general": "general_prompt.yaml",
     "general_reasoning": "general_prompt_with_reasoning.yaml"
-    #"rsa": "rsa_prompt.yaml",
 }
 
 SHARED_SEEDS = 1
