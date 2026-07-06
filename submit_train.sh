58af5823c288a0df2a6273e5f41827a7abeb3188
GOESTERN-1147554 <u27871@glogin10.usr.hpc.gwdg.de>
2026-07-06 17:20:24 +0200

    update files

--- submit_train.sh
+++ submit_train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name="mca-irony"
-#SBATCH --time=24:00:00
+#SBATCH --time=1:00:00
 #SBATCH --ntasks=1
 #SBATCH --threads-per-core=1
-#SBATCH --mem=40G
-#SBATCH -p kisski
-#SBATCH -G A100:1
+#SBATCH --mem=150
+#SBATCH -p kisski-h100
+#SBATCH -G H100:1
 
 module load spack
 spack load miniconda
