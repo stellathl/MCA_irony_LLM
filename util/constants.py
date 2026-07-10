b4757171ff5b70d92df33ef4d29b929117354c4c
Rabia Çiftçi <rabiaciftci@Rabia-MacBook-Air.local>
2026-07-10 16:33:29 +0200

    to restore to its original state

--- util/constants.py
+++ util/constants.py
@@ -1,20 +1,20 @@
-# MODELS = {
-#     "Gemma-3-4B":          "google/gemma-3-4b-it",
-#     "Mistral-7B-Instruct": "mistralai/Mistral-7B-Instruct-v0.3",
-#     "OLMo-2-7B":           "allenai/OLMo-2-1124-7B-Instruct",
-#     "Qwen3-8B":            "Qwen/Qwen3-8B",
-#     "Gpt2-1.5B":            "openai-community/gpt2-xl",
-#     "Llama-3-8B":          "meta-llama/Llama-3.1-8B-Instruct"
-# }
-
 MODELS = {
-    "Gemma-3-1B": "google/gemma-3-1b-it"
- }
+    "Gemma-3-4B":          "google/gemma-3-4b-it",
+    "Mistral-7B-Instruct": "mistralai/Mistral-7B-Instruct-v0.3",
+    "OLMo-2-7B":           "allenai/OLMo-2-1124-7B-Instruct",
+    "Qwen3-8B":            "Qwen/Qwen3-8B",
+    "Gpt2-1.5B":            "openai-community/gpt2-xl",
+    "Llama-3-8B":          "meta-llama/Llama-3.1-8B-Instruct"
+}
+
+# MODELS = {
+#     "Gemma-3-1B": "google/gemma-3-1b-it"
+#  }
 
 PROMPT_FILES = {
     "rsa": "rsa_prompt.yaml",
-    #  "general": "general_prompt.yaml",
-    #  "general_reasoning": "general_prompt_with_reasoning.yaml"
+     "general": "general_prompt.yaml",
+      "general_reasoning": "general_prompt_with_reasoning.yaml"
 }
 
 SHARED_SEEDS = 1
