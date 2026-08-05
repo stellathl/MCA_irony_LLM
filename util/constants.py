05af4f934d01f2834e532ee97a2409684117a746
Asuka Kuwahara <42548220+asukakuwahara@users.noreply.github.com>
2026-08-05 13:01:16 +0200

    Update bash file and requirements.txt for hpc, only for Gemma and Mistral (#33)
    * update bash file and requirements.txt for hpc
    
    * wip, ran general/reasoning template with gemma 3 1B model
    
    * fix merge conflictwq
    
    * update files
    
    * update the logs
    
    * update to use cashed model after install, use gpu!
    
    * complete running for gemma 3 4B and mistral 7B!
    
    * add visualization
    
    * run all the models with temperature 0.1
    
    * fix merge conflict
    
    * clean up
    
    * revert the change to avoid merge conflict, since it'll be overridden
    
    ---------
    
    Co-authored-by: GOESTERN-1147554 <u27871@glogin9.usr.hpc.gwdg.de>
    Co-authored-by: GOESTERN-1147554 <u27871@glogin10.usr.hpc.gwdg.de>

--- util/constants.py
+++ util/constants.py
@@ -13,8 +13,8 @@ MODELS = {
 
 PROMPT_FILES = {
     "rsa": "rsa_prompt.yaml",
-     "general": "general_prompt.yaml",
-      "general_reasoning": "general_prompt_with_reasoning.yaml"
+    "general": "general_prompt.yaml",
+    "general_reasoning": "general_prompt_with_reasoning.yaml"
 }
 
 SHARED_SEEDS = 1
